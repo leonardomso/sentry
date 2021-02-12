bf659e0d06ed8ae8a363a7bf0656c672fb91fcaa
Armen <armenzg@sentry.io>
2021-02-12 13:15:40 -0500

    Debug info

--- scripts/pyenv_setup.sh
+++ scripts/pyenv_setup.sh
@@ -109,8 +109,11 @@ setup_pyenv() {
   # If the script is called with the "dot space right" approach (. ./scripts/pyenv_setup.sh),
   # the effects of this will be persistent outside of this script
   echo "Activating pyenv"
+  set +x
+  python -V
   eval "$(pyenv init -)"
   python_version=$(python -V | sed s/Python\ //g)
+  echo "${python_version}"
   [[ $python_version != $(cat .python-version) ]] && echo "Wrong Python version: $python_version" && exit 1
 }
 
